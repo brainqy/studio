8a3c2b793c1f2cec4e5cb975500eea0a0267fb9c
Dnyanesh <somwanshi.dnyaneshwar@gmail.com>
2025-05-16 07:57:51 +0000

    I see this error with the app, reported by NextJS, please fix it. The error is reported as HTML but presented visually to the user).
    A > before the line number in the error source usually indicates the line of interest:
    
    > ./src/app/(app)/events/page.tsx:39:4
    > Parsing ecmascript source code failed
    >   37 | }
    >   38 |
    > > 39 | ```
    >      |    ^
    >
    > Unexpected eof
    >
    >

--- src/app/(app)/events/page.tsx
+++ src/app/(app)/events/page.tsx
@@ -1,5 +1,4 @@
 
-
 "use client";
 
 import { Card, CardContent, CardHeader, CardTitle, CardDescription } from "@/components/ui/card";
@@ -35,5 +34,3 @@ export default function EventsPage() {
     </div>
   );
 }
-
-```
